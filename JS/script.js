9e2179f2206c5eac272ce52b88d6f9c9e49c0159
Otabek Rakhimov <rahimovmail@gmail.com>
2022-11-04 11:47:50 -0700

    Edit html and css

--- JS/script.js
+++ JS/script.js
@@ -1,41 +1,44 @@
 ///////////////////////////////////////////////////////////
 // Set current year
-const yearEl = document.querySelector(".year");
-const currentYear = new Date().getFullYear();
-yearEl.textContent = currentYear;
+console.log("hello world");
 
-const headerElement = document.querySelector("#header");
-const navElement = document.querySelector(".nav");
-let options = {
-  root: null,
-  rootMargin: `-150px`,
-  threshold: 0,
-};
+// const headerElement = document.querySelector("#header");
+// const navElement = document.querySelector(".nav");
+// let options = {
+//   root: null,
+//   rootMargin: `-150px`,
+//   threshold: 0,
+// };
 
-function stickyNavigation(entries) {
-  let ent = entries[0];
+// function stickyNavigation(entries) {
+//   let ent = entries[0];
 
-  if (ent.isIntersecting === false) {
-    navElement.classList.add("sticky");
-  }
+//   if (ent.isIntersecting === false) {
+//     navElement.classList.add("sticky");
+//   }
 
-  if (ent.isIntersecting === true) {
-    navElement.classList.remove("sticky");
-  }
-}
+//   if (ent.isIntersecting === true) {
+//     navElement.classList.remove("sticky");
+//   }
+// }
 
-const observer = new IntersectionObserver(stickyNavigation, options);
+// const observer = new IntersectionObserver(stickyNavigation, options);
 
-observer.observe(headerElement);
+// observer.observe(headerElement);
 
-const menuIcon = document.querySelector("#menu-icon");
-const closeIcon = document.querySelector("#close-icon");
-const mobileNav = document.querySelector(".mobile-nav");
+document.addEventListener("DOMContentLoaded", function (e) {
+  const menuIcon = document.querySelector("#hamburger-icon");
+  const closeIcon = document.querySelector("#close-icon");
+  const mobileNav = document.querySelector(".mobile-nav");
 
-menuIcon.addEventListener("click", function () {
-  mobileNav.classList.add("mobile-nav--show");
-});
+  menuIcon.addEventListener("click", function () {
+    mobileNav.classList.add("mobile-nav--show");
+  });
 
-closeIcon.addEventListener("click", function () {
-  mobileNav.classList.remove("mobile-nav--show");
+  closeIcon.addEventListener("click", function () {
+    mobileNav.classList.remove("mobile-nav--show");
+  });
+  const yearEl = document.querySelector(".year");
+  const currentYear = new Date().getFullYear();
+  yearEl.textContent = currentYear;
 });
